cc656a6cb4e64948305096898626fb34499fb1f0
Chase Brown <rappybrown@gmail.com>
2020-10-23 21:09:26 -0400

    importing parse

--- cloud/main.js
+++ cloud/main.js
@@ -1,1 +1,6 @@
+import Parse from 'parse';
+
+Parse.initialize("69f63a6eacfc2ad0fa74ba8cc7258f42", null, "2cead4ea95dc3747e92edde72c3dfa6e");
+Parse.serverURL = "https://startup-5676.nodechef.com/parse";
+
 const app = require('./app.js');
