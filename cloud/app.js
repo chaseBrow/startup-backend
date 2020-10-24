1b869b66bede9382b5f88d949e844a6b1f5ab318
Chase Brown <rappybrown@gmail.com>
2020-10-23 21:35:08 -0400

    done

--- cloud/app.js
+++ cloud/app.js
@@ -2,10 +2,17 @@ const Parse = require('parse/node');
 
 app.get("/", (req, res) => {
     if (req) {
-        res.send("heard you loud and clear");
+        console.log(req.query.test);
+        res.send("req");
     }
     else {
         res.send("Hello my name is bobby");
     }
 });
+
+app.post("/", (req, res) => {
+    console.log(req);
+    res.send("login");
+});
+
 console.log("Hello world") 
